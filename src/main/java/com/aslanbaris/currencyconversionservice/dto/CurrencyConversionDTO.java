9d3d79d330aec8335995457d900fcf5ed67e5672
Barış Aslan <aslannbaris@gmail.com>
2020-12-13 22:56:10 +0300

    initial

---
+++ src/main/java/com/aslanbaris/currencyconversionservice/dto/CurrencyConversionDTO.java
@@ -0,0 +1,22 @@
+package com.aslanbaris.currencyconversionservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyConversionDTO {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private int port;
+}
