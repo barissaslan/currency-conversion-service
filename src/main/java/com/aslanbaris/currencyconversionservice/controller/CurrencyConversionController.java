443d655135458151e3b91213a3bd441fe70a0e81
Barış Aslan <aslannbaris@gmail.com>
2020-12-14 22:31:53 +0300

    eureka naming server

--- src/main/java/com/aslanbaris/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/aslanbaris/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,25 +1,23 @@
 package com.aslanbaris.currencyconversionservice.controller;
 
+import com.aslanbaris.currencyconversionservice.CurrencyExchangeServiceProxy;
 import com.aslanbaris.currencyconversionservice.dto.CurrencyConversionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionDTO convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-        CurrencyConversionDTO response = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionDTO.class, uriVariables).getBody();
+        CurrencyConversionDTO response = proxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversionDTO(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
