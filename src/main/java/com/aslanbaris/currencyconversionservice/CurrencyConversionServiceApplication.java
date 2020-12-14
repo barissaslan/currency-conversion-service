a74817df5da174cd41b2ac62cb8254f60190c8c6
Barış Aslan <aslannbaris@gmail.com>
2020-12-14 23:15:04 +0300

    update

--- src/main/java/com/aslanbaris/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/aslanbaris/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.aslanbaris.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients("com.aslanbaris.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
