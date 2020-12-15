e4d8d32a0964c8f0e8552aa212574cce9f74961f
Barış Aslan <aslannbaris@gmail.com>
2020-12-16 00:18:32 +0300

    update

--- src/main/java/com/aslanbaris/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/aslanbaris/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,7 @@ package com.aslanbaris.currencyconversionservice.controller;
 
 import com.aslanbaris.currencyconversionservice.CurrencyExchangeServiceProxy;
 import com.aslanbaris.currencyconversionservice.dto.CurrencyConversionDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RestController
 public class CurrencyConversionController {
 
@@ -18,6 +20,8 @@ public class CurrencyConversionController {
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionDTO convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         CurrencyConversionDTO response = proxy.retrieveExchangeValue(from, to);
+
+        log.info("response: {}", response);
 
         return new CurrencyConversionDTO(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
